78e94414d7e88e54588a57f17126cc5b8b049e28
jjhyunjung <jihyunj22@naver.com>
2022-07-20 01:19:50 +0900

    [Add] subtotal 장바구니 개수, 총액 기능 구현

--- src/NavBar/Sections/Header.js
+++ src/NavBar/Sections/Header.js
@@ -3,8 +3,11 @@ import './Header.css'
 import SearchIcon from '@material-ui/icons/Search'
 import ShoppingBasket from '@material-ui/icons/ShoppingBasket'
 import {Link} from 'react-router-dom'
+import { useStateValue } from '../../StateProvider'
 
 function Header() {
+  const [{basket}, dispatch] = useStateValue(); 
+
   return (
     <div className='header'>
         <Link to="/">
@@ -25,7 +28,7 @@ function Header() {
             <div className='header_optionBasket'>
                 <ShoppingBasket />
                 <span className='header_optionLineTwoheader_basketCount'>
-                0
+                    {basket?.length}
                 </span>
             </div>
         </Link>
